67e053be196a13d2aa5b61401f95c66fbbc6ca15
Mark Rees <mark.rees@dwp.gsi.gov.uk>
2016-03-21 15:23:16 +0000

    bower in gulp, removed bower_components

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,15 @@
 'use strict';
 
-var gulp = require('gulp');
-var nodemon = require('gulp-nodemon');
+var gulp = require('gulp'),
+  nodemon = require('gulp-nodemon'),
+  bower = require('gulp-bower');
 
 gulp.task('start-node', function() {
-	return nodemon({
-		script: 'server.js'
-	});
-}) ;
+  return nodemon({
+    script: 'server.js'
+  });
+});
+
+gulp.task('bower', function() {
+  return bower();
+});
