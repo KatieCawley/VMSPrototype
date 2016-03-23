a07e6627d0e7cfbb0f5477c24b52f8cf22c46bc5
Katie Cawley <katie.cawley.dwp@gmail.com>
2016-03-23 11:38:13 +0000

    Jasmine unit testing

--- gulp/tasks/unit_test.js
+++ gulp/tasks/unit_test.js
@@ -1,9 +1,21 @@
 const istanbul = require('gulp-istanbul'),
       gulp = require('gulp'),
-      jasmine = require('gulp-jasmine'),
+      gulpJasmine = require('gulp-jasmine'),
+      Jasmine = require('jasmine'),
+      reporters = require('jasmine-reporters'),
       istanbulOptions = {
         includeUntested:true
-      };
+      },
+      junitReporters = (reportsDir) =>[
+        new reporters.JUnitXmlReporter({
+          savePath: reportsDir,
+          consolidateAll: false
+        }),
+        new Jasmine.ConsoleReporter({
+          print: console.log
+        })
+      ];
+
 
 gulp.task('instrument-app', () => {
   return gulp.src('app/**.js')
@@ -13,6 +25,8 @@ gulp.task('instrument-app', () => {
 
 gulp.task('unit', ['instrument-app'], () =>{
   gulp.src('test/app/**.js')
-  .pipe(jasmine({}))
+  .pipe(gulpJasmine({
+    reporter: junitReporters('reports/unit')
+  }))
   .pipe(istanbul.writeReports(istanbulOptions));
 });
