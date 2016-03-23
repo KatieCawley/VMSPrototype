15c9e6ef81f1c02fdf38a791ec5387208101d754
Katie Cawley <katie.cawley.dwp@gmail.com>
2016-03-23 11:19:40 +0000

    IstanbulOption

--- gulp/tasks/unit_test.js
+++ gulp/tasks/unit_test.js
@@ -1,15 +1,18 @@
 const istanbul = require('gulp-istanbul'),
       gulp = require('gulp'),
-      jasmine = require('gulp-jasmine');
+      jasmine = require('gulp-jasmine'),
+      istanbulOptions = {
+        includeUntested:true
+      };
 
 gulp.task('instrument-app', () => {
   return gulp.src('app/**.js')
-  .pipe(istanbul({}))
+  .pipe(istanbul(istanbulOptions))
   .pipe(istanbul.hookRequire());
 });
 
 gulp.task('unit', ['instrument-app'], () =>{
   gulp.src('test/app/**.js')
   .pipe(jasmine({}))
-  .pipe(istanbul.writeReports({}));
+  .pipe(istanbul.writeReports(istanbulOptions));
 });
